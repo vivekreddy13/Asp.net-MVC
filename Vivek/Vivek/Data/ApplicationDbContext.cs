23973b0a953a8c5ec16404e73e694d0e712013f8
vivekreddy13 <vivekreddy2999@gmail.com>
2021-01-30 13:29:41 +0530

    My Frst Web App using Asp.net

---
+++ Vivek/Vivek/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vivek.Models;
+
+namespace Vivek.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ Vivek/Vivek/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vivek.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
